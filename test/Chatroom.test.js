8b452633de70712301be15520d47df7fd63578d5
Bryce <bechampaign@hotmail.com>
2019-02-04 22:11:35 -0500

    Configured test setup with web3 and compiled chatroom contract

--- test/Chatroom.test.js
+++ test/Chatroom.test.js
@@ -2,8 +2,8 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-
-const { interface, bytecode } = require('../compile');
+const compiledChatroom = require('../ethereum/build/Chatroom.json');
+const chatroomOutput = compiledChatroom['Chatroom']['Chatroom'];
 
 let chatroom;
 let accounts;
@@ -11,8 +11,8 @@ let accounts;
 beforeEach (async () => {
   accounts = await web3.eth.getAccounts();
 
-  chatroom = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode})
+  chatroom = await new web3.eth.Contract(chatroomOutput.abi)
+    .deploy({ data: '0x' + chatroomOutput.evm.bytecode.object})
     .send({ from: accounts[0], gas: 6700000 });
 });
 
