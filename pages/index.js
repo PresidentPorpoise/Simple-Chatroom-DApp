53b6f28c57e986570c22a48b903a74fde9a15ad4
Bryce <bechampaign@hotmail.com>
2019-02-04 22:13:14 -0500

    Set up placeholder index page content

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,9 @@
 import React, { Component } from 'react';
 import { Card } from 'semantic-ui-react';
+import chatroom from '../ethereum/chatroom.js';
+
+render() {
+  <div>
+    <h1>Chatroom Application</h1>
+  </div>
+}
