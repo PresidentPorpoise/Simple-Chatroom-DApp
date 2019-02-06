26c57c69b8ac18221f1e6ef693306fc5d706cbaf
PresidentPorpoise <bechampaign@hotmail.com>
2019-02-05 23:05:02 -0500

    Common header component for pages

---
+++ components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Menu, Icon, Input, Button } from 'semantic-ui-react';
+
+export default () => {
+  return (
+    <Menu style={{ marginTop: '10px'}} size='medium'>
+      <Menu.Item>
+        <Icon name='home' />
+        Chatroom
+      </Menu.Item>
+
+      <Menu.Item>
+        <Icon name='github' />
+        Code
+      </Menu.Item>
+
+      <Menu.Item position="right">
+        <Icon name='question circle' />
+        About
+      </Menu.Item>
+    </Menu>
+  )
+}
