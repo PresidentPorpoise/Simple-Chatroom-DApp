0fad991a02cd38aeecd9625c8e8bb4bbd154d852
PresidentPorpoise <bechampaign@hotmail.com>
2019-03-23 17:49:52 -0400

    added responsive header, changed header styling

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,53 @@
-import React from 'react';
-import { Menu, Icon, Input, Button } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Menu, Icon, Input, Button, Responsive, Dropdown } from 'semantic-ui-react';
+import { Link } from '../routes.js';
+import Head from 'next/head';
+import '../components/styles.css';
 
-export default () => {
-  return (
-    <Menu style={{ marginTop: '10px'}} size='medium' secondary>
-      <Menu.Item>
-        <Icon name='home' />
-        Chatroom
-      </Menu.Item>
+class Header extends Component {
+  state = { visible: false }
 
-      <Menu.Item position="right">
-        <Icon name='github' />
-        Code
-      </Menu.Item>
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>Chatroom DApp</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+        </Head>
+          <Responsive minWidth={800}>
+            <Menu secondary className="navbar-items">
+              <Menu.Item header>
+                <Link href='/'><a>Chatroom DApp</a></Link>
+              </Menu.Item>
 
-      <Menu.Item>
-        <Icon name='question circle' />
-        About
-      </Menu.Item>
-    </Menu>
-  )
+              <Menu.Item  position="right" href='/about'>
+                About
+              </Menu.Item>
+              <Menu.Item href='/about'>
+                Github
+              </Menu.Item>
+            </Menu>
+          </Responsive>
+
+          <Responsive maxWidth={799}>
+            <Menu secondary className="navbar-items">
+              <Menu.Item header>
+                <Link href='/'><a>Chatroom</a></Link>
+              </Menu.Item>
+
+              <Menu.Item  position="right">
+                <Dropdown icon='bars' direction='left'>
+                  <Dropdown.Menu>
+                    <Dropdown.Item  href='/about' text='About' />
+                    <Dropdown.Item  href='/' text='Github' />
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Menu.Item>
+            </Menu>
+          </Responsive>
+        </div>
+    )
+  }
 }
+
+export default Header;
