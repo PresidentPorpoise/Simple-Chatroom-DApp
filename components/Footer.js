e3e031f7d8fc4d4aa9f054dfbc943fa867361042
PresidentPorpoise <bechampaign@hotmail.com>
2019-03-24 00:55:31 -0400

    Populated footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from '../routes';
+import { Icon } from 'semantic-ui-react';
 
 class Footer extends Component {
   render() {
     return(
       <footer>
         <div id='footer'>
-          <p>Developed by Bryce Champaign<br/><Link href='https://www.brycechampaign.me'><a>www.brycechampaign.me</a></Link></p>
+          <div style={{ paddingTop: '0.5em' }}>
+            <p>Developed by Bryce Champaign<br/>
+            <Link href='https://www.brycechampaign.me'>
+            <a>www.brycechampaign.me</a></Link></p>
+          </div>
+          <Link href='https://github.com/PresidentPorpoise/Simple-Chatroom-DApp'>
+          <a id='iconLink'><Icon name='github' floated='right' size='large' link />
+          </a></Link>
         </div>
       </footer>
     );
