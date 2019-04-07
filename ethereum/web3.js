a7e04b20b923e488d7bc65ed2b73ab4b05916696
PresidentPorpoise <bechampaign@hotmail.com>
2019-04-06 20:20:47 -0400

    Updated infura api key

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -6,7 +6,7 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   web3 = new Web3(window.web3.currentProvider);
 } else {
   const provider = new Web3.providers.HttpProvider(
-    'https://rinkeby.infura.io/v3/68231853f39c4e27bdcd83c2d6ca0a94'
+    'https://rinkeby.infura.io/v3/5aadf6de28f54290be64561d04566f0f'
   );
   web3 = new Web3(provider);
 }
